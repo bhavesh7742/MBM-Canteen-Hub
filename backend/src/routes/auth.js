53635686b33d220ccb0b204c5c2cbc3fdba028bd
BHAVESH KUMAR <bhaveshsiwana2004@gmail.com>
2026-03-15 00:17:52 +0530

    add vercel.json

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
 const { register, login, adminLogin, getMe } = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
 
-router.post('/api/auth/register', register);
-router.post('/api/auth/login', login);
+router.post('/register', register);
+router.post('/login', login);
 router.post('/admin/login', adminLogin);
 router.get('/me', protect, getMe);
 
